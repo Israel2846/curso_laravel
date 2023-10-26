1b62243dfa64c762ea72ac0b260c31c1d63b5f91
Israel Colín <israaacolin@gmail.com>
2023-10-25 22:26:52 -0600

    Ejercicio de migraciones

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.plantilla')
 @section('title', 'Home')
 @section('content')
-    <h1>Bienvenido al curso <?php echo $curso; ?></h1>
+    <h1>Bienvenido al curso {{ $curso }}</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', HomeController::class);
 Route::controller(CursoController::class)->group(function () {
     Route::get('cursos', 'index');
     Route::get('cursos/create', 'create');
-    Route::get('cursos/{show}', 'show');
+    Route::get('cursos/{curso}', 'show');
 });
 
 /* Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
