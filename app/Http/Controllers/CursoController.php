8a98472b6e31e6f475ee26111ea903cdab667a05
Israel Colín <israaacolin@gmail.com>
2023-10-27 02:24:01 -0600

    Listando cursos

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 use Psy\CodeCleaner\ReturnTypePass;
 
@@ -9,7 +10,9 @@ class CursoController extends Controller
 {
     public function index()
     {
-        return view('cursos.index');
+        $cursos = Curso::paginate();
+
+        return view('cursos.index', compact('cursos'));
     }
 
     public function create()
--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.plantilla')
 @section('title', 'Index')
 @section('content')
+
     <h1>Bienvenido a la página de cursos</h1>
+    <a href="{{ route('cursos.create') }}">Crear curso</a>
+
+    <ul>
+        @foreach ($cursos as $curso)
+            <li>{{ $curso->name }}</li>
+        @endforeach
+    </ul>
+
+    {{ $cursos->links() }}
+
 @endsection
