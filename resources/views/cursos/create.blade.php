a700722de3d78c9e1dde8467d86932a19d2753a4
Israel Colín <israaacolin@gmail.com>
2023-10-27 18:39:30 -0600

    Agregando validaciones y lenguaje español

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -8,22 +8,37 @@
         <label>
             Nombre:
             <br>
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{ old('name') }}">
         </label>
+        @error('name')
+            <br>
+            <span>*{{ $message }}</span>
+            <br>
+        @enderror
         <br>
 
         <label>
             Descripción:
             <br>
-            <textarea name="descripcion" rows="5"></textarea>
+            <textarea name="descripcion" rows="5">{{ old('descripcion') }}</textarea>
         </label>
+        @error('descripcion')
+            <br>
+            <span>*{{ $message }}</span>
+            <br>
+        @enderror
         <br>
 
         <label>
             Categoría:
             <br>
-            <input type="text" name="categoria">
+            <input type="text" name="categoria" value="{{ old('categoria') }}">
         </label>
+        @error('categoria')
+            <br>
+            <span>*{{ $message }}</span>
+            <br>
+        @enderror
         <br>
 
         <button type="submit">Enviar</button>
